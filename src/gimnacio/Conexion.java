7b068eed1c3e1d9718281e771718386ac0273a06
liooficial <jjuli123.jm456@gmail.com>
2022-07-22 17:09:06 -0500

    nueva version con cliente

---
+++ src/gimnacio/Conexion.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gimnacio;
+import java.sql.*;
+import java.sql.Connection;
+/**
+ *
+ * @author liooficial
+ */
+public class Conexion {
+    public static Connection getConexion() {
+        String connectionUrl ="jdbc:sqlserver://localhost:1433;databaseName=GYM1;integratedSecurity=true";
+                //+"databaseName=GYM1;"+"user=sa"+"password=12345"+"loginTimeout=30";
+                //"integratedSecurity=true";
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            System.out.println("Driver funciona correctamente.");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        try {
+            Connection con = DriverManager.getConnection(connectionUrl);
+            return con;
+        } catch (SQLException ex) {
+            System.out.println("Error........:"+ex.toString());
+             return null;
+        }
+        
+    }
+
+}
